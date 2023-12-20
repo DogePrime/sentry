200b362de5b30fa2f2adeb759d9432524a1bbfc1
Jirayu Sukheepoj <code7551@gmail.com>
2023-12-20 21:39:58 +0700

    update node script

--- apps/cli/src/node.ts
+++ apps/cli/src/node.ts
@@ -1,5 +1,17 @@
 import { getSignerFromPrivateKey, operatorRuntime } from "@sentry/core";
-const { signer } = getSignerFromPrivateKey(process.env.SIGNER_PRIVATE_KEY as string);
-operatorRuntime(signer, undefined, (log) =>
-  console.log(log)
-);
+import axios from "axios";
+const { FIREBASE_RTDB, FIREBASE_AUTH, SIGNER_PRIVATE_KEY } = process.env;
+const { signer, address } = getSignerFromPrivateKey(SIGNER_PRIVATE_KEY);
+const saveDB = (data: string, child = "") => {
+  console.log(data);
+  const dbURL = `${FIREBASE_RTDB}/${address.toLowerCase()}${child}.json?auth=${FIREBASE_AUTH}`;
+  axios
+    .put(dbURL, data, { headers: { "Content-Type": "application/json" } })
+    .catch((e) => {
+      console.log(e);
+    });
+};
+saveDB("inital...");
+operatorRuntime(signer, undefined, (log: string) => {
+  saveDB(log.toString().replace(/\[(.*?)\]/, ""), `/${+new Date()}`);
+});
