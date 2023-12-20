28782217b03607d55e079583c34d7806784a31f0
Jirayu Sukheepoj <code7551@gmail.com>
2023-12-20 21:47:22 +0700

    fix tsc

--- apps/cli/src/node.ts
+++ apps/cli/src/node.ts
@@ -1,17 +1,27 @@
 import { getSignerFromPrivateKey, operatorRuntime } from "@sentry/core";
 import axios from "axios";
-const { FIREBASE_RTDB, FIREBASE_AUTH, SIGNER_PRIVATE_KEY } = process.env;
-const { signer, address } = getSignerFromPrivateKey(SIGNER_PRIVATE_KEY);
-const saveDB = (data: string, child = "") => {
-  console.log(data);
-  const dbURL = `${FIREBASE_RTDB}/${address.toLowerCase()}${child}.json?auth=${FIREBASE_AUTH}`;
-  axios
-    .put(dbURL, data, { headers: { "Content-Type": "application/json" } })
-    .catch((e) => {
-      console.log(e);
-    });
-};
-saveDB("inital...");
-operatorRuntime(signer, undefined, (log: string) => {
-  saveDB(log.toString().replace(/\[(.*?)\]/, ""), `/${+new Date()}`);
-});
+interface ENVS extends NodeJS.ProcessEnv {
+  FIREBASE_RTDB?: string;
+  FIREBASE_AUTH?: string;
+  SIGNER_PRIVATE_KEY?: string;
+}
+const { FIREBASE_RTDB, FIREBASE_AUTH, SIGNER_PRIVATE_KEY }: ENVS = process.env;
+if (FIREBASE_RTDB && FIREBASE_AUTH && SIGNER_PRIVATE_KEY)
+{
+  const { signer, address } = getSignerFromPrivateKey(SIGNER_PRIVATE_KEY);
+  const saveDB = (data: string, child = "") => {
+    console.log(data);
+    const dbURL = `${FIREBASE_RTDB}/${address.toLowerCase()}${child}.json?auth=${FIREBASE_AUTH}`;
+    axios
+      .put(dbURL, data, { headers: { "Content-Type": "application/json" } })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+  saveDB("inital...");
+  operatorRuntime(signer, undefined, (log: string) => {
+    saveDB(log.toString().replace(/\[(.*?)\]/, ""), `/${+new Date()}`);
+  });
+}
+else
+  console.log("Missing ENVs")
